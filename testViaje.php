913d2aa1a0038391545890f0513a9d33b3a49b31
Araceli Mondaca <80856098+AraceliMondaca@users.noreply.github.com>
2022-04-03 20:13:38 -0300

    Create testViaje.php

---
+++ testViaje.php
@@ -0,0 +1,120 @@
+<?php 
+//include_once'Viaje.php';
+require_once ('Viaje.php');
+echo "¡Información del Pasajero! \n";
+
+echo"Ingrese nombre: \n";
+$nombre=trim(fgets(STDIN))."\n";
+echo"/--------------------------------/\n";
+echo"Ingrese el apellido: \n";
+$apellido=trim(fgets(STDIN))."\n";
+echo"/--------------------------------/\n";
+echo"Ingrese el DNI: \n";
+$numDocumento=trim(fgets(STDIN))."\n";
+echo "¡Información del Viaje! \n";
+echo"/--------------------------------/\n";
+echo"Ingrese destino: \n";
+$destino=trim(fgets(STDIN))."\n";
+echo"/-------------------------------/\n";
+echo"Ingrese codigo del viaje: \n";
+$codigo=trim(fgets(STDIN))."\n";
+
+$objViaje=new Viaje($nombre,$apellido,$numDocumento,$destino,$codigo);
+
+/**
+ * es un string con el menu de opciones que pude realizar el cliente
+ * @return int
+ */
+function menu()
+{
+     $menu="--------------------Opciones-------------------\n";
+      "opcion 1 "."\n"."Agregar pasajero: \n";
+      "opcion 2 "."\n"."Quitar pasajero: \n";
+      "opcion 3 "."\n"."Modificar pasajero: \n";
+      "opcion 4 "."\n"."Ver viaje :\n";
+      "opcion 5 "."\n"."Modificar el destino del viaje: \n";
+      "opcion 6 "."\n"."Modificar el codigo : \n"; 
+      "opcion 7 "."\n"."Modificar la cantidad de pasajes : \n";
+      "opcion 8"."\n"."salir.\n";
+     
+     
+return $menu;
+}
+/**
+ * obtiene los datos y los devuelve en un arreglo
+ */
+function obtenerDatos(){
+    echo"DNI\n";
+    $numeDocumento=strval(trim(fgets(STDIN)))."\n";
+    echo"Nombre\n";
+    $nombre=trim(fgets(STDIN))."\n";
+    echo"Apellido\n";
+    $apellido=trim(fgets(STDIN))."\n";
+    $pasajero=["DNI"=>$numeDocumento,"Nombre"=>$nombre,"Apellido"=>$apellido];
+    return $pasajero;
+}
+$ejecucion=true;
+do {
+    print menu();
+    $opc=trim(fgets(STDIN));
+    switch ($opc==1) {
+        case 'opcion 1':
+            if($objViaje->PasajesDisponibles()){
+                echo "Ingrese los datos del pasajero: \n";
+                $pasajero = obtenerDatos();
+                echo $objViaje->incremetar($pasajero);
+            }else{
+                echo "No hay mas pasajes en este viaje.\n";
+            }            
+            break;
+         
+        case 'opcion 2':
+                echo "Ingrese los datos del pasajero a quitar: \n";
+                $pasajero = obtenerDatos();
+                echo $objViaje->reducir($pasajero);
+                break;
+
+        case 'opcion 3':
+                 echo "Ingrese los datos del pasajero a modificar: \n";
+                 $pasajero = obtenerDatos();
+                 echo "Ingrese los nuevos datos: \n";
+                 $otroPasajero = obtenerDatos();
+                 echo $objViaje->modificarDatos($pasajero, $otroPasajero);
+                 break;
+
+        case 'opcion 4':
+                 echo $objViaje;
+                 break;
+
+        case 'opcion 5':
+            echo "El viaje con destino a: {$objViaje->getDestino()}. \n";
+            echo "Ingrese el nuevo destino: \n";
+            $destino = trim(fgets(STDIN));
+            $objViaje->setDestino($destino);
+            break;
+        
+        
+       case 'opcion 5':
+                echo "El viaje posee el código: {$objViaje->getCodigo()}. \n";
+                echo "Ingrese el nuevo código: \n";
+                $codigo = trim(fgets(STDIN));
+                $codigo = intval($codigo);
+                $objViaje->setCodigo($codigo);
+                break;
+        
+        case 'opcion 6':
+            echo "El viaje posee {$objViaje->getCantidadMaxPasajeros()} de pasajes. \n";
+            echo "Ingrese la nueva cantidad de pasajes: \n";
+            $cantidadAsientos = trim(fgets(STDIN));
+            $cantidadAsientos = intval($cantidadAsientos);
+            $objViaje->setCantidadMaxPasajeros($cantidadAsientos);
+            break;
+
+        default:
+            $ejecucion=false;
+            break;
+    }
+} while ($ejecucion == true);
+
+
+?>
